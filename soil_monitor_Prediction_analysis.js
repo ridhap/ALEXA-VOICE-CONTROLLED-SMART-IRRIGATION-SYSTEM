4d1f735d0946c113cd25e3381243dc932f349678
Ridha P <42196007+ridhap@users.noreply.github.com>
2021-01-02 18:04:37 +0530

    Update soil_monitor_Prediction_analysis.js

--- soil_monitor_Prediction_analysis.js
+++ soil_monitor_Prediction_analysis.js
@@ -1,1 +1,6 @@
-soil_monitor
+setChartLibrary('google-chart');
+setChartTitle('Polynomial Regression');
+setChartType('predictionGraph');
+setAxisName('time_stamp','soil');
+mul(0.0977);
+plotChart('time_stamp','soil');
